b8e90918bdd19972591fefc60f9d20ad28080e8a
Megan Forgey <meganforgey@Megans-MacBook-Air.local>
2020-01-19 11:16:48 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+main.js
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
