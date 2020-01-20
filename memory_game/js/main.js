435c94c8e1c649126af40446d7c5a829cd0158ca
Megan Forgey <meganforgey@Megans-MacBook-Air.local>
2020-01-20 06:58:52 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -39,11 +39,10 @@ function checkForMatch() {
 		}
 	}
 };
-let cardsInPlay = [];
 
 function createBoard() {
 	for (var i =0; i < cards.length; i++){
-		const cardElement = document.createElement('img');
+		let cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard);
@@ -51,12 +50,29 @@ function createBoard() {
 	}
 };
 function flipCard(){
-	const cardId = this.getAttribute('data-id');
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch;
+if(cardInPlay.length === 2){
+	checkForMatch();
+}
+
 };
+function createBoard(){
+for (let i = 0; i < cards.length, i++){
+	let cardElement = document.createElement('img');
+	cardElement.setAttribute('Back', 'images.back.png');
+	cardElement.setAttribute('data-id', i);
+	document.getElementById('game-board').appendChild(cardElement);
+
+	cardElement.addEventListener('click', flipCard);
+}
+};
+createBoard();
+
+<script type="text/javascript" src="js/main.js"> </script>
 
 
