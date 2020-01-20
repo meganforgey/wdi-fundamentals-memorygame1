a2027bd982319b2fb02e1b0378347fce2d8a763e
Megan Forgey <meganforgey@Megans-MacBook-Air.local>
2020-01-20 06:42:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,27 @@
 main.js
-console.log("Up and running!");
 
-
-
-console.log("User flipped " + cardOne);
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-
-let cardOne = [0];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+];
 
 cardsInPlay,push(cardOne);
 console.log("User flipped " + cardOne);
@@ -16,3 +29,34 @@ console.log("User flipped " + cardOne);
 let cardTwo = [2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry try again.");
+		}
+	}
+};
+let cardsInPlay = [];
+
+function createBoard() {
+	for (var i =0; i < cards.length; i++){
+		const cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+};
+function flipCard(){
+	const cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	checkForMatch;
+};
+
+
